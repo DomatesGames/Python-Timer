0ed60b069ada607db1f9d6ebaf97a921b51429d2
Domates_Games <74550237+DomatesGames@users.noreply.github.com>
2021-03-03 20:11:24 +0200

    Update Tomato-Timer.py

--- Tomato-Timer.py
+++ Tomato-Timer.py
@@ -4,7 +4,7 @@ author = "DomatesGames from Github"
 hour = int(input("Hour? "))
 hour = hour*60*60
 minute = int(input("Minute?? "))
-minute = dakika*60
+minute = minute*60
 second = int(input("Second? "))
 second = second
 time.sleep(hour + minute + second)
